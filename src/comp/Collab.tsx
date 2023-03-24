a82957ed829c24a76316bc576f6f09c1c5263090
cuvar <luca-mueller@gmx.net>
2023-03-24 12:02:53 +0100

    fix: div instead of fragment

--- src/comp/Collab.tsx
+++ src/comp/Collab.tsx
@@ -2,9 +2,9 @@ import Editor from "./Editor";
 
 export default function Collab() {
   return (
-    <>
+    <div className="flex flex-col items-center space-y-4">
       <h1 className="text-2xl font-bold">Collab mode</h1>
       <Editor />
-    </>
+    </div>
   );
 }
