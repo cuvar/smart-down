665f012ed15a59beb03d5c94604097e2972247ef
tsch4k0mo <jakobbraun6@gmail.com>
2023-04-20 10:52:56 +0200

    feat: back button

--- src/comp/Collab.tsx
+++ src/comp/Collab.tsx
@@ -1,8 +1,29 @@
 import Editor from "./Editor";
 
 export default function Collab() {
+  const backIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="h-6 w-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+      />
+    </svg>
+  );
+
   return (
     <div className="flex flex-col items-center space-y-4">
+      <a href="/" className="flex space-x-2">
+        <span>{backIcon}</span>
+        <span>back</span>
+      </a>
       <h1 className="text-2xl font-bold">Collab mode</h1>
       <Editor />
     </div>
